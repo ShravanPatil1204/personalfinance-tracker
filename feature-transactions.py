b0ecdc1694578e3696ddece67bec3f2f79e99ca7
ShravanPatil1204 <shravanpatil2794@gmail.com>
2026-04-23 15:02:37 +0530

    Member1: Added expense function

--- feature-transactions.py
+++ feature-transactions.py
@@ -10,3 +10,12 @@ def add_income(data):
         "date": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     })
 
+def add_expense(data):
+    amount = float(input("Enter expense amount: "))
+    category = input("Enter category: ")
+    data.append({
+        "type": "Expense",
+        "amount": amount,
+        "category": category,
+        "date": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    })
